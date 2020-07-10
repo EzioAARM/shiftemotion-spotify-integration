60c22c768b923559be60c19131ac7b944a5627e1
Davidov249 <31377711+Davidov249@users.noreply.github.com>
2020-07-10 00:15:11 -0600

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default() 
 	
 	r.GET("/", func(c *gin.Context){
-		c.String(200, "Servicio Levantado con cicd!", nil)
+		c.String(200, "Servicio Levantado implementando cicd!", nil)
 	})
 
 	r.Run(":3000") 
