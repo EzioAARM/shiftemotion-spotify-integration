e08b254869e80adbca9576f07f8fb58e2dec98eb
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-12 12:51:49 -0600

    Cambios de prueba para stress test

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-
-		c.String(200, "Servicio Levantado utilizando AWS codepipeline! (Ah perroooooooooooooooooooo!)", nil)
+		time.Sleep(1 * time.Second)
+		var suma = 0
+		for i := 0; i <= 150000; i++ {
+			suma += i
+		}
+		c.String(200, "Servicio Levantado utilizando AWS codepipeline! (Ah perroooooooooooooooooooo!) "+strconv.Itoa(suma), nil)
 
 	})
 
