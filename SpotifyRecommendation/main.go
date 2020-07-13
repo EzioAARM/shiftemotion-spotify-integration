54619fd399d1d7ed7684cf09be5f544999d93a3f
Luis Fernando Marroquin Martinez <luisfer.marroquin@hotmail.com>
2020-07-13 16:42:14 -0600

    Removed Keys

--- SpotifyRecommendation/main.go
+++ SpotifyRecommendation/main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"../SpotifyRecommendation/routes"
-	"os"
 )
 
 func main() {
 	r := routes.RegisterRoutes()
 
-	// Testing purposes
-	setEnvVariables()
+	// Testing purpos
 
 	r.Run(":3000")
 }
 
 // Comment this function before pushing and getting in prod
-func setEnvVariables() {
-	os.Setenv("AWS_ACCESS_KEY_ID", "AKIA5WKCWMCB4COQMP5A")
-	os.Setenv("AWS_SECRET_ACCESS_KEY", "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")
-	os.Setenv("S3_IMAGE_BUCKET", "shift-emotion-pictures")
-	os.Setenv("REGION", "us-east-2")
-}
