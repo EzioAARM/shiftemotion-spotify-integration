2d7dc9ba4c1a6d5255c355af5dac51ca49f83b96
luis-marro <luisfer.marroquin1@gmail.com>
2020-07-18 10:47:01 -0600

    Added the emotion field to the output json.

--- SpotifyRecommendation/main.go
+++ SpotifyRecommendation/main.go
@@ -17,4 +17,8 @@ func main() {
 // Comment this function before pushing and getting in production
 func setEnvVariables() {
 	os.Setenv("GIN_MODE", "release")
+	os.Setenv("AWS_ACCESS_KEY_ID", "AKIA5WKCWMCB6VVGY4E5")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "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")
+	os.Setenv("S3_IMAGE_BUCKET", "shiftemotion-pictures")
+	os.Setenv("REGION", "us-east-1")
 }
